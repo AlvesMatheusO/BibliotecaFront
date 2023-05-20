559bd71c4825be106b864ce43bf43dbbee0298bd
Matheus Alves <matheusalv.oliveira@gmail.com>
2023-05-19 21:26:37 -0300

    receving data from api mocked

---
+++ src/main/java/org/example/Livro.java
@@ -0,0 +1,129 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livro {
+
+    private long id;
+    private String titulo;
+    private double preco;
+    private String descricao;
+    private String autor;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public static void consultarLivros() {
+
+        try {
+            String apiUrl = "http://localhost:8080/livros";
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Código de resposta: " + responseCode);
+
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+
+            // Converte a resposta para um array de objetos JSON
+            JsonArray jsonArray = new Gson().fromJson(response.toString(), JsonArray.class);
+
+            // Cria uma lista para armazenar os objetos Livro
+            List<Livro> listaLivros = new ArrayList<>();
+
+            // Itera sobre os objetos do array e adiciona-os à lista
+            for (JsonElement elemento : jsonArray) {
+                JsonObject livroJson = elemento.getAsJsonObject();
+                long id = livroJson.get("id").getAsLong();
+                String titulo = livroJson.get("titulo").getAsString();
+                String descricao = livroJson.get("descricao").getAsString();
+                double preco = livroJson.get("preco").getAsDouble();
+                String autor = livroJson.get("autor").getAsString();
+
+                // Cria um objeto Livro e adiciona-o à lista
+                Livro livro = new Livro();
+                livro.setId(id);
+                livro.setTitulo(titulo);
+                livro.setAutor(autor);
+                livro.setPreco(preco);
+                livro.setDescricao(descricao);
+                listaLivros.add(livro);
+            }
+
+            // Exibe a lista de livros
+            for (Livro livro : listaLivros) {
+                System.out.println("ID: " + livro.getId());
+                System.out.println("Titulo: " + livro.getTitulo());
+                System.out.println("Descricao: " + livro.getDescricao());
+                System.out.println("Preco: " + livro.getPreco());
+                System.out.println("Autor: " + livro.getAutor());
+                System.out.println(" ");
+            }
+
+            // Fecha a conexão
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
