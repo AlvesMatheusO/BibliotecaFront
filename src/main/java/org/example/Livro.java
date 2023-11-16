5d871d42d6129d3beea456b7f8731aa158371864
AlvesOliveiraMatheus <matheusalv.oliveira@gmail.com>
2023-11-16 16:22:03 -0300

    fixes

--- src/main/java/org/example/Livro.java
+++ src/main/java/org/example/Livro.java
@@ -71,7 +71,6 @@ public class Livro {
             String apiUrl = "http://localhost:8080/livros";
 
             URL url = new URL(apiUrl);
-
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
             connection.setRequestMethod("GET");
@@ -79,10 +78,9 @@ public class Livro {
             int responseCode = connection.getResponseCode();
             System.out.println("Código de resposta: " + responseCode);
 
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
             StringBuilder response = new StringBuilder();
+            String inputLine;
 
             while ((inputLine = reader.readLine()) != null) {
                 response.append(inputLine);
@@ -92,15 +90,14 @@ public class Livro {
             // Converte a resposta para um array de objetos JSON
             JsonArray jsonArray = new Gson().fromJson(response.toString(), JsonArray.class);
 
-
             // Itera sobre os objetos do array e adiciona-os à lista
             for (JsonElement elemento : jsonArray) {
                 JsonObject livroJson = elemento.getAsJsonObject();
                 long id = livroJson.get("id").getAsLong();
-                String titulo = livroJson.get("titulo").isJsonNull() ? null : livroJson.get("titulo").getAsString();
-                String descricao = livroJson.get("descricao").isJsonNull() ? null : livroJson.get("descricao").getAsString();
-                double preco = livroJson.get("preco").getAsDouble();
-                String autor = livroJson.get("autor").isJsonNull() ? null : livroJson.get("autor").getAsString();
+                String titulo = livroJson.has("titulo") ? livroJson.get("titulo").getAsString() : "";
+                String descricao = livroJson.has("descricao") ? livroJson.get("descricao").getAsString() : "";
+                double preco = livroJson.has("preco") ? livroJson.get("preco").getAsDouble() : 0.0;
+                String autor = livroJson.has("autor") ? livroJson.get("autor").getAsString() : "";
 
                 // Cria um objeto Livro e adiciona-o à lista
                 Livro livro = new Livro();
@@ -111,8 +108,6 @@ public class Livro {
                 livro.setDescricao(descricao);
                 listaLivros.add(livro);
             }
-            setListaLivros(listaLivros);
-
 
             // Exibe a lista de livros
             for (Livro livro : listaLivros) {
@@ -124,7 +119,6 @@ public class Livro {
                 System.out.println(" ");
             }
 
-            // Fecha a conexão
             connection.disconnect();
         } catch (IOException e) {
             e.printStackTrace();
@@ -139,45 +133,48 @@ public class Livro {
             String apiUrl = "http://localhost:8080/livros?titulo=" + titulo;
 
             URL url = new URL(apiUrl);
-
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
             System.out.println("Código de resposta: " + responseCode);
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
             StringBuilder response = new StringBuilder();
+            String inputLine;
 
             while ((inputLine = reader.readLine()) != null) {
                 response.append(inputLine);
             }
             reader.close();
 
-            // Converte a resposta para um objeto JSON
             JsonObject jsonResponse = new Gson().fromJson(response.toString(), JsonObject.class);
 
-            // Obtém a matriz de objetos "livros" do JSON
-            JsonArray livrosArray = jsonResponse.getAsJsonArray("livros");
+            // Verifica se o elemento "livros" existe no JSON response
+            if (jsonResponse.has("livros")) {
+                JsonArray livrosArray = jsonResponse.getAsJsonArray("livros");
 
-            // Itera sobre os objetos da matriz e adiciona-os à lista
-            for (JsonElement elemento : livrosArray) {
-                long id = elemento.getAsJsonObject().get("id").getAsLong();
-                String tituloLivro = elemento.getAsJsonObject().get("titulo").getAsString();
-                String descricao = elemento.getAsJsonObject().get("descricao").getAsString();
-                double preco = elemento.getAsJsonObject().get("preco").getAsDouble();
-                String autor = elemento.getAsJsonObject().get("autor").getAsString();
+                // Itera sobre os objetos da matriz e adiciona-os à lista
+                for (JsonElement elemento : livrosArray) {
+                    JsonObject livroJson = elemento.getAsJsonObject();
+                    long id = livroJson.get("id").getAsLong();
+                    String tituloLivro = livroJson.get("titulo").getAsString();
+                    String descricao = livroJson.get("descricao").getAsString();
+                    double preco = livroJson.get("preco").getAsDouble();
+                    String autor = livroJson.get("autor").getAsString();
 
-                // Cria um objeto Livro e adiciona-o à lista
-                Livro livro = new Livro();
-                livro.setId(id);
-                livro.setTitulo(tituloLivro);
-                livro.setAutor(autor);
-                livro.setPreco(preco);
-                livro.setDescricao(descricao);
-                livrosFiltrados.add(livro);
+                    // Cria um objeto Livro e adiciona-o à lista
+                    Livro livro = new Livro();
+                    livro.setId(id);
+                    livro.setTitulo(tituloLivro);
+                    livro.setAutor(autor);
+                    livro.setPreco(preco);
+                    livro.setDescricao(descricao);
+                    livrosFiltrados.add(livro);
+                }
+            } else {
+                // Lidar com o caso em que "livros" não está presente ou é nulo
+                System.out.println("O elemento 'livros' não está presente no JSON retornado.");
             }
 
             // Fecha a conexão
